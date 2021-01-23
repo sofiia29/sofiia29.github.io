fa3239e99472903838ff04ffc98d02aa2311a4cd
sofiia29 <75383648+sofiia29@users.noreply.github.com>
2021-01-23 12:03:20 +0200

    checking win, configure buttons

--- game.js
+++ game.js
@@ -7,12 +7,13 @@ var result = document.getElementById('result');
 var circles = 6;
 var colors = []; 
 var pickedcolor;
+var bool = false;
 //генерує випадковий колір
 function generate_color(){ 
 	let r = Math.floor(Math.random() * 256);
 	let g = Math.floor(Math.random() * 256);
 	let b = Math.floor(Math.random() * 256);
-	let res = 'rgb' + '(' + r + ',' + g + ',' + b + ')'
+	let res = 'rgb' + '(' + r + ', ' + g + ', ' + b + ')'
 	return res;
 }
 //створює список кольорів, залежно від кількості кружечків
@@ -29,7 +30,13 @@ function new_game(){
 	var n = Math.floor(Math.random() * circles);
 	pickedcolor = colors[n]; //вибраний колір
 	rgb.textContent = pickedcolor;
-	result.textContent = '';
+	if( bool == true){
+		result.textContent = '';
+		bool = false;
+	}
+	else{
+		result.textContent = 'try again';
+	}
 	for (var i = 0; i < squares.length; i++) {
 		if (i < circles){
 			squares[i].style.display = 'block';
@@ -40,22 +47,52 @@ function new_game(){
 		}
 	}
 }
-new_game()
 
-function easy_or_hard(){
-	if(easy.style.color == '#b3b3ff'){
+
+
+function change_col(but){
+	if(but.id == 'easy'){
+		easy.style.color = '#b3b3ff';
+		hard.style.color = '#000099';
 		circles = 3;
 	}
+
 	else{
+		hard.style.color = '#b3b3ff';
+		easy.style.color = '#000099';
 		circles = 6;
 	}
 }
 
-function change_col(but){
-	if(but.id == easy){
-		easy.style.color == '#b3b3ff';
-	}
-	else{
-		hard.style.color == '#b3b3ff';
+function correct_or_not(){
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].addEventListener('click', function(){
+			if(this.style.backgroundColor === pickedcolor){
+				result.textContent = 'correct';
+				paint_all()
+				bool = true;
+			}
+			else{
+				result.textContent = 'try again';
+				new_game()
+				bool = false;
+			}
+		})
 	}
 }
+
+
+function paint_all(){
+	let body = document.getElementsByTagName('h1')[0];
+	body.style.backgroundColor = pickedcolor;
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = pickedcolor;
+	}
+}
+
+function start_game(){
+	new_game()
+	result.textContent = '';
+	correct_or_not()
+}
+start_game()
